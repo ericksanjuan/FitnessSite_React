902fd61a8b0cae47ac27afcd29b6db5ea69cdc4b
Michelle Showers <michelle.showers.ga72@statefarm.com>
2023-03-20 14:11:27 -0500

    Add Header.js component

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+
+    return (
+        <nav className='header'>
+            <h1>TEAM FIT: WORKOUT GENERATOR</h1>
+        </nav>
+    )
+}
+
+export default Header;
