87cc71a7c8dbe14aa604a15944b8ccabb23a7915
Bob Nelson1971 <rln71@att.net>
2023-03-20 20:24:37 -0500

    made some changes to the render menu items for review in the am

--- src/component/UserInput.js
+++ src/component/UserInput.js
@@ -5,23 +5,34 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-function UserInput({bodyPartDD, setBodyParts, bodyPart}) {
+function UserInput({bodyPartDD, setBodyPart, bodyPart}) {
   
-
+// there was a typo here that I also corrected on setBodyPart(we had it originally as setBodyParts)
   const handleChange = (event) => {
-    setBodyParts(event.target.value);
+    setBodyPart(event.target.value);
   };
 
 //   const renderMenuItems = bodyPartDD.map(item => {
 //     <MenuItem value={item.}
 //   })
 
-  function renderMenuItems (bodyPartDD) {
-    for (const entry of bodyPartDD) {
-        <MenuItem value={entry}>`${entry.stringify}`</MenuItem>
-      }
+  // function renderMenuItems (bodyPartDD) {
+  //   for (const entry of bodyPartDD) {
+  //       <MenuItem value={entry}>{entry.stringify}</MenuItem>
+       
+  //     }
       
-  }
+  // }
+  // Bob changed this last night. Making the bodypartDD an array allowed the use of map functions. 
+  function renderMenuItems(bodyPartDD) {
+    console.log('renderMenuItems called with', bodyPartDD);
+    const bodyPartArray = Array.from(bodyPartDD)
+  return [ <MenuItem key="empty" value=""></MenuItem>, // Add empty value
+  ...bodyPartArray.map((entry, index) => (
+    <MenuItem key={index} value={entry}>{entry}</MenuItem>
+  ))
+];
+}
 
   return (
     <Box sx={{ minWidth: 120 }}>
@@ -31,11 +42,11 @@ function UserInput({bodyPartDD, setBodyParts, bodyPart}) {
           labelId="bodyPart"
           id="bodyPart"
           value= {bodyPart}
-          label="bodyPart"
+          label="Body Part"
           onChange={handleChange}
         >  
-         {renderMenuItems}
-          {/* <MenuItem value={bodyPartDD[0]}>Waist</MenuItem> */}
+         {renderMenuItems(bodyPartDD)}
+        {/* <MenuItem value={bodyPartDD[0]}>Waist</MenuItem> */}
         </Select>
       </FormControl>
     </Box>
