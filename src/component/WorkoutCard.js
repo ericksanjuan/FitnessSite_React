a13f605ca9ed3e8c86d64a111f2b16b2f97f01ad
Michelle Showers <michelle.showers.ga72@statefarm.com>
2023-03-22 14:25:49 -0500

    Styling changes

--- src/component/WorkoutCard.js
+++ src/component/WorkoutCard.js
@@ -7,7 +7,6 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import ShareBtn from "./ShareBtn";
 
-
 function WorkoutCard({ workOutCard }) {
   const [page, setPage] = useState(1);
   const workoutsPerPage = 3;
@@ -26,9 +25,9 @@ function WorkoutCard({ workOutCard }) {
     <Stack>
       <Stack direction="row" spacing={2} sx={{ padding: 2, justifyContent:"center" }}>
       {workOutCard.slice((page - 1) * workoutsPerPage, page * workoutsPerPage).map((workout) => (
-        <Card sx={{ maxWidth: 345 }} key={workout.id}>
+        <Card sx={{ maxWidth: 345, minWidth: 320}} key={workout.id}>
           <CardMedia
-            sx={{ height: 200 }}
+            sx={{ height: 350 }}
             image={workout.gifUrl} 
             title={workout.name}
           />
@@ -37,7 +36,13 @@ function WorkoutCard({ workOutCard }) {
               {workout.name.replace(/^./, workout.name[0].toUpperCase())}
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              {workout.bodyPart}
+              Body Part: {workout.bodyPart}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Muscle Type: {workout.target}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Equipment Needed: {workout.equipment}
             </Typography>
           </CardContent>
           <CardActions>
