d875a3545d68660b0a522669ee880e348c2ada39
Erick San Juan <esanjuanalvarez@outlook.com>
2023-03-22 10:21:01 -0500

    added pagination to cards and limited them to 3 a page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Header from "./component/Header";
 import UserInput from "./component/UserInput";
 import { Button, Container, Paper, Stack } from "@mui/material";
 import WorkoutCard from "./component/WorkoutCard";
-// import Pagination from "./component/Pagination";
-import MuiPagination from "./component/MuiPagination"
 
 function App() {
   // DD means DropDown
@@ -76,9 +74,11 @@ function App() {
           </Stack>
         </Paper>
       </Container>
-      <WorkoutCard workOutCard={workOutCard} />
-      {/* <Pagination itemsPerPage={3} workOutCard={workOutCard} /> */}
-      <MuiPagination />
+      <Container>
+        <Stack direction="row" spacing={2} sx={{ padding: 2, justifyContent:"center" }}>
+          <WorkoutCard workOutCard={workOutCard} />
+        </Stack>
+      </Container>
     </div>
   );
 }
--- src/component/WorkoutCard.js
+++ src/component/WorkoutCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "@mui/material";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -7,36 +7,54 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 
-
 function WorkoutCard({ workOutCard }) {
-  console.log('card data:', workOutCard)
+  const [page, setPage] = useState(1);
+  const workoutsPerPage = 3;
+  const totalWorkouts = workOutCard.length;
+  const totalPages = Math.ceil(totalWorkouts / workoutsPerPage);
+ 
+  const handlePrevClick = () => {
+    setPage(prevPage => prevPage - 1);
+  };
 
-  const threeWorkouts = workOutCard.slice(0,3)
-  const nextThree = 
+  const handleNextClick = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
   return (
-    threeWorkouts.map((workout) => (
-      <Card sx={{ maxWidth: 345 }} key={workout.id}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image={workout.gifUrl}
-          title={workout.name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {workout.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {workout.bodyPart}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-    ))
+    <>
+      {workOutCard.slice((page - 1) * workoutsPerPage, page * workoutsPerPage).map((workout) => (
+        <Card sx={{ maxWidth: 345 }} key={workout.id}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={workout.gifUrl}
+            title={workout.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {workout.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {workout.bodyPart}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>))}
+        {totalWorkouts > workoutsPerPage && (
+        <div>
+          <Button onClick={handlePrevClick} disabled={page === 1}>
+            Prev
+          </Button>
+          <Button onClick={handleNextClick} disabled={page === totalPages}>
+            Next
+          </Button>
+        </div>
+      )}
+    </>
   );
 }
 
-export default WorkoutCard;
+export default WorkoutCard 
